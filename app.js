201a0809271a9e5a4e4c0bbf6cbe9c2c0d1a1ce2
Garrukzijian <u6128492@au.edu>
2022-02-28 19:26:14 +0700

    add the database fuction

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
+require('./db/db.js')
 
+// Load routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var customerRouter = require('./routes/customers');
 
 var app = express();
 
+app.use(cors());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +27,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Plug routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/customers',customerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
