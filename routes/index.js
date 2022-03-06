8a148d3a759d6383734d91a7c37c7b8d508dc6ac
Garrukzijian <u6128492@au.edu>
2022-03-07 01:59:29 +0700

    change the mongobd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// const UserModel = require('../db/models/UserModel')
+// const CategoryModel = require('../db/models/CategoryModel')
+// const ProductModel = require('../db/models/ProductModel')
+// const RoleModel = require('../db/models/RoleModel')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chayapol' });
+  res.render('index', { title: 'Garruk' });
 });
 
 module.exports = router;
