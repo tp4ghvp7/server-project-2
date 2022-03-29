d3c7a7bf6d7bbc651c41be0625459e1ea0582aa4
Zijian Zhou <86515498+Garrukzijian@users.noreply.github.com>
2022-03-30 01:53:22 +0700

    finish  develop

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
-/*
-用来定义路由的路由器模块
- */
+
 const express = require('express')
 const md5 = require('blueimp-md5')
 
@@ -10,26 +8,19 @@ const ProductModel = require('../db/models/ProductModel')
 const RoleModel = require('../db/models/RoleModel')
 
 
-// 得到路由器对象
 const router = express.Router()
-// console.log('router', router)
 
-// 指定需要过滤的属性
 const filter = {password: 0, __v: 0}
-/* GET home page. */
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Garruk'});
 });
 
-// 登陆
 router.post('/login', (req, res) => {
   const {username, password} = req.body
-  // 根据username和password查询数据库users, 如果没有, 返回提示错误的信息, 如果有, 返回登陆成功信息(包含user)
   UserModel.findOne({username, password: md5(password)})
     .then(user => {
-      if (user) { // 登陆成功
-        // 生成一个cookie(userid: user._id), 并交给浏览器保存
+      if (user) { 
         res.cookie('userid', user._id, {maxAge: 1000 * 60 * 60 * 24})
         if (user.role_id) {
           RoleModel.findOne({_id: user.role_id})
@@ -40,11 +31,10 @@ router.post('/login', (req, res) => {
             })
         } else {
           user._doc.role = {menus: []}
-          // 返回登陆成功信息(包含user)
           res.send({status: 0, data: user})
         }
 
-      } else {// 登陆失败
+      } else {
         res.send({status: 1, msg: 'username or password not correct!'})
       }
     })
@@ -54,52 +44,41 @@ router.post('/login', (req, res) => {
     })
 })
 
-// 添加用户
 router.post('/manage/user/add', (req, res) => {
-  // 读取请求参数数据
   const {username, password} = req.body
-  // 处理: 判断用户是否已经存在, 如果存在, 返回提示错误的信息, 如果不存在, 保存
-  // 查询(根据username)
   UserModel.findOne({username})
     .then(user => {
-      // 如果user有值(已存在)
       if (user) {
-        // 返回提示错误的信息
-        res.send({status: 1, msg: '此用户已存在'})
+        res.send({status: 1, msg: 'already have this user'})
         return new Promise(() => {
         })
-      } else { // 没值(不存在)
-        // 保存
+      } else {
         return UserModel.create({...req.body, password: md5(password || 'atguigu')})
       }
     })
     .then(user => {
-      // 返回包含user的json数据
       res.send({status: 0, data: user})
     })
     .catch(error => {
-      console.error('注册异常', error)
-      res.send({status: 1, msg: '添加用户异常, 请重新尝试'})
+      console.error('register error', error)
+      res.send({status: 1, msg: 'register error please try again'})
     })
 })
 
 
-// 更新用户
 router.post('/manage/user/update', (req, res) => {
   const user = req.body
   UserModel.findOneAndUpdate({_id: user._id}, user)
     .then(oldUser => {
       const data = Object.assign(oldUser, user)
-      // 返回
       res.send({status: 0, data})
     })
     .catch(error => {
-      console.error('更新用户异常', error)
-      res.send({status: 1, msg: '更新用户异常, 请重新尝试'})
+      console.error('update faile', error)
+      res.send({status: 1, msg: 'update faile'})
     })
 })
 
-// 删除用户
 router.post('/manage/user/delete', (req, res) => {
   const {userId} = req.body
   UserModel.deleteOne({_id: userId})
@@ -108,32 +87,6 @@ router.post('/manage/user/delete', (req, res) => {
     })
 })
 
-// 获取用户信息的路由(根据cookie中的userid)
-/*router.get('/user', (req, res) => {
-  // 从请求的cookie得到userid
-  const userid = req.cookies.userid
-  // 如果不存在, 直接返回一个提示信息
-  if (!userid) {
-    return res.send({status: 1, msg: '请先登陆'})
-  }
-  // 根据userid查询对应的user
-  UserModel.findOne({_id: userid}, filter)
-    .then(user => {
-      if (user) {
-        res.send({status: 0, data: user})
-      } else {
-        // 通知浏览器删除userid cookie
-        res.clearCookie('userid')
-        res.send({status: 1, msg: '请先登陆'})
-      }
-    })
-    .catch(error => {
-      console.error('获取用户异常', error)
-      res.send({status: 1, msg: '获取用户异常, 请重新尝试'})
-    })
-})*/
-
-// 获取所有用户列表
 router.get('/manage/user/list', (req, res) => {
   UserModel.find({username: {'$ne': 'admin'}})
     .then(users => {
@@ -142,13 +95,11 @@ router.get('/manage/user/list', (req, res) => {
       })
     })
     .catch(error => {
-      console.error('获取用户列表异常', error)
-      res.send({status: 1, msg: '获取用户列表异常, 请重新尝试'})
+      console.error('get user list faile', error)
+      res.send({status: 1, msg: 'get user list faile'})
     })
 })
 
-
-// 添加分类
 router.post('/manage/category/add', (req, res) => {
   const {categoryName, parentId} = req.body
   CategoryModel.create({name: categoryName, parentId: parentId || '0'})
@@ -156,12 +107,11 @@ router.post('/manage/category/add', (req, res) => {
       res.send({status: 0, data: category})
     })
     .catch(error => {
-      console.error('添加分类异常', error)
-      res.send({status: 1, msg: '添加分类异常, 请重新尝试'})
+      console.error('add category falie', error)
+      res.send({status: 1, msg: 'add category falie'})
     })
 })
 
-// 获取分类列表
 router.get('/manage/category/list', (req, res) => {
   const parentId = req.query.parentId || '0'
   CategoryModel.find({parentId})
@@ -169,12 +119,11 @@ router.get('/manage/category/list', (req, res) => {
       res.send({status: 0, data: categorys})
     })
     .catch(error => {
-      console.error('获取分类列表异常', error)
-      res.send({status: 1, msg: '获取分类列表异常, 请重新尝试'})
+      console.error('get category list faile', error)
+      res.send({status: 1, msg: 'get category list faile'})
     })
 })
 
-// 更新分类名称
 router.post('/manage/category/update', (req, res) => {
   const {categoryId, categoryName} = req.body
   CategoryModel.findOneAndUpdate({_id: categoryId}, {name: categoryName})
@@ -182,12 +131,11 @@ router.post('/manage/category/update', (req, res) => {
       res.send({status: 0})
     })
     .catch(error => {
-      console.error('更新分类名称异常', error)
-      res.send({status: 1, msg: '更新分类名称异常, 请重新尝试'})
+      console.error('update category name faile', error)
+      res.send({status: 1, msg: 'update category name faile'})
     })
 })
 
-// 根据分类ID获取分类
 router.get('/manage/category/info', (req, res) => {
   const categoryId = req.query.categoryId
   CategoryModel.findOne({_id: categoryId})
@@ -195,13 +143,12 @@ router.get('/manage/category/info', (req, res) => {
       res.send({status: 0, data: category})
     })
     .catch(error => {
-      console.error('获取分类信息异常', error)
-      res.send({status: 1, msg: '获取分类信息异常, 请重新尝试'})
+      console.error('get category information faile', error)
+      res.send({status: 1, msg: 'get category information faile'})
     })
 })
 
 
-// 添加产品
 router.post('/manage/product/add', (req, res) => {
   const product = req.body
   ProductModel.create(product)
@@ -209,12 +156,11 @@ router.post('/manage/product/add', (req, res) => {
       res.send({status: 0, data: product})
     })
     .catch(error => {
-      console.error('添加产品异常', error)
-      res.send({status: 1, msg: '添加产品异常, 请重新尝试'})
+      console.error('add product faile', error)
+      res.send({status: 1, msg: 'add product faile'})
     })
 })
 
-// 获取产品分页列表
 router.get('/manage/product/list', (req, res) => {
   const {pageNum, pageSize} = req.query
   ProductModel.find({})
@@ -222,12 +168,11 @@ router.get('/manage/product/list', (req, res) => {
       res.send({status: 0, data: pageFilter(products, pageNum, pageSize)})
     })
     .catch(error => {
-      console.error('获取商品列表异常', error)
-      res.send({status: 1, msg: '获取商品列表异常, 请重新尝试'})
+      console.error('get product list faile', error)
+      res.send({status: 1, msg: 'get product list faile'})
     })
 })
 
-// 搜索产品列表
 router.get('/manage/product/search', (req, res) => {
   const {pageNum, pageSize, searchName, productName, productDesc} = req.query
   let contition = {}
@@ -241,12 +186,11 @@ router.get('/manage/product/search', (req, res) => {
       res.send({status: 0, data: pageFilter(products, pageNum, pageSize)})
     })
     .catch(error => {
-      console.error('搜索商品列表异常', error)
-      res.send({status: 1, msg: '搜索商品列表异常, 请重新尝试'})
+      console.error('search product faile', error)
+      res.send({status: 1, msg: 'search product faile'})
     })
 })
 
-// 更新产品
 router.post('/manage/product/update', (req, res) => {
   const product = req.body
   ProductModel.findOneAndUpdate({_id: product._id}, product)
@@ -254,12 +198,11 @@ router.post('/manage/product/update', (req, res) => {
       res.send({status: 0})
     })
     .catch(error => {
-      console.error('更新商品异常', error)
-      res.send({status: 1, msg: '更新商品名称异常, 请重新尝试'})
+      console.error('update product faile', error)
+      res.send({status: 1, msg: 'update product faile'})
     })
 })
 
-// 更新产品状态(上架/下架)
 router.post('/manage/product/updateStatus', (req, res) => {
   const {productId, status} = req.body
   ProductModel.findOneAndUpdate({_id: productId}, {status})
@@ -267,13 +210,12 @@ router.post('/manage/product/updateStatus', (req, res) => {
       res.send({status: 0})
     })
     .catch(error => {
-      console.error('更新产品状态异常', error)
-      res.send({status: 1, msg: '更新产品状态异常, 请重新尝试'})
+      console.error('update product condition faile', error)
+      res.send({status: 1, msg: 'update product condition faile'})
     })
 })
 
 
-// 添加角色
 router.post('/manage/role/add', (req, res) => {
   const {roleName} = req.body
   RoleModel.create({name: roleName})
@@ -281,42 +223,36 @@ router.post('/manage/role/add', (req, res) => {
       res.send({status: 0, data: role})
     })
     .catch(error => {
-      console.error('添加角色异常', error)
-      res.send({status: 1, msg: '添加角色异常, 请重新尝试'})
+      console.error('add role faile', error)
+      res.send({status: 1, msg: 'add role faile'})
     })
 })
 
-// 获取角色列表
 router.get('/manage/role/list', (req, res) => {
   RoleModel.find()
     .then(roles => {
       res.send({status: 0, data: roles})
     })
     .catch(error => {
-      console.error('获取角色列表异常', error)
-      res.send({status: 1, msg: '获取角色列表异常, 请重新尝试'})
+      console.error('get role list faile', error)
+      res.send({status: 1, msg: 'get role list faile'})
     })
 })
 
-// 更新角色(设置权限)
+
 router.post('/manage/role/update', (req, res) => {
   const role = req.body
   role.auth_time = Date.now()
   RoleModel.findOneAndUpdate({_id: role._id}, role)
     .then(oldRole => {
-      // console.log('---', oldRole._doc)
       res.send({status: 0, data: {...oldRole._doc, ...role}})
     })
     .catch(error => {
-      console.error('更新角色异常', error)
-      res.send({status: 1, msg: '更新角色异常, 请重新尝试'})
+      console.error('update role faile', error)
+      res.send({status: 1, msg: 'update role faile'})
     })
 })
 
-
-/*
-得到指定数组的分页信息对象
- */
 function pageFilter(arr, pageNum, pageSize) {
   pageNum = pageNum * 1
   pageSize = pageSize * 1
